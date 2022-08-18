4f3ca945af9585433dc07f058896d746979ef401
Rohan Orton <rohan.orton@gmail.com>
2022-08-18 15:29:03 +0200

    changes to null-ls config

--- lua/rohan/plugins/null-ls.lua
+++ lua/rohan/plugins/null-ls.lua
@@ -1,11 +1,16 @@
-local null_ls = require('null-ls')
+local null_ls = require("null-ls")
+
+-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
+local formatting = null_ls.builtins.formatting
+-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/code_actions
+local code_actions = null_ls.builtins.code_actions
 
 null_ls.setup({
+  debug = false,
   sources = {
-    -- null_ls.builtins.formatting.stylua,
-    -- null_ls.builtins.diagnostics.eslint,
-    -- null_ls.builtins.completion.spell,
-    null_ls.builtins.code_actions.refactoring,
-    null_ls.builtins.formatting.rustfmt,
+    code_actions.refactoring,
+    formatting.rustfmt,
+    formatting.prettier,
+    formatting.stylua,
   },
 })
