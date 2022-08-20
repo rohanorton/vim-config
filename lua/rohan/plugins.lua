9e77c2186692e35e8f2597bfefeb35c15a081d11
Rohan Orton <rohan.orton@gmail.com>
2022-08-20 09:35:44 +0200

    add lsp signature help to cmp

--- lua/rohan/plugins.lua
+++ lua/rohan/plugins.lua
@@ -56,6 +56,7 @@ return packer.startup(function(use)
 		"hrsh7th/cmp-buffer",
 		"hrsh7th/cmp-path",
 		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-nvim-lsp-signature-help",
 		"hrsh7th/cmp-nvim-lua",
 		"saadparwaiz1/cmp_luasnip",
 	})
