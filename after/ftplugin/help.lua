51474a6c8a9c9f211517bbc26259c632cea2b52a
Rohan Orton <rohan.orton@gmail.com>
2022-10-12 00:08:36 +0200

    Get rid of zoomed help
    It's annoying. I want fullscreen help, but not 100% of the time. Also it
    fails frequently.

--- after/ftplugin/help.lua
+++ after/ftplugin/help.lua
@@ -1,9 +1,2 @@
 -- Close buffer using q
 vim.keymap.set("n", "q", "<Cmd>bdelete<CR>", { buffer = true })
-
--- Start help in fullscreen buffer.
--- FIXME: If you are already in a zoomed buffer, this errors. Not sure what the
---        best approach to fixing this is, so just moving this to the end of
---        the file so that when it fails it doesn't break the rest of the
---        config
-vim.cmd("ZoomWinTabIn")
