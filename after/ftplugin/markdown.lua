b203a7aa9f35e6c83cc218ec488f72dc0db8ae55
Rohan Orton <rohan.orton@gmail.com>
2022-10-11 21:55:32 +0200

    Update format options for markdown

---
+++ after/ftplugin/markdown.lua
@@ -0,0 +1,7 @@
+vim.bo.formatoptions = ""
+  .. "t" -- Auto-wrap
+  .. "a" -- Auto-format paragraphs
+  .. "c" -- Auto-wrap comments
+  .. "q" -- Allow comment formatting with gq
+  .. "l" -- Break long lines in insert mode
+  .. "n" -- Indent numbered lists
