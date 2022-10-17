2d34d81610b2a46a6a621b257d82279ce071a00b
Rohan Orton <rohan.orton@gmail.com>
2022-10-18 01:45:10 +0200

    Update startup screen

--- plugin/alpha.lua
+++ plugin/alpha.lua
@@ -1,5 +1,59 @@
 SAFE_REQUIRE({ "alpha", "alpha.themes.startify" }, function(alpha, startify)
-  alpha.setup(startify.opts)
+  startify.section.header = {
+    type = "text",
+    val = {
+      [[                                                        neovim.  ]],
+      [[  ,---,---,---,---,---,---,---,---,---,---,---,---,---,-------,  ]],
+      [[  |1/2| 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 0 | + | ' | <-    |  ]],
+      [[  |---'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-----|  ]],
+      [[  | ->| | Q | W | E | R | T | Y | U | I | O | P | ] | ^ |  ↵  |  ]],
+      [[  |-----',--',--',--',--',--',--',--',--',--',--',--',--'|    |  ]],
+      [[  | Caps | A | S | D | F | G | H | J | K | L | \ | [ | * |    |  ]],
+      [[  |----,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'---'----|  ]],
+      [[  |    | < | Z | X | C | V | B | N | M | , | . | - |          |  ]],
+      [[  |----'-,-',--'--,'---'---'---'---'---'---'-,-'---',--,------|  ]],
+      [[  | ctrl |  | alt |                          |altgr |  | ctrl |  ]],
+      [[  '------'  '-----'--------------------------'------'  '------'  ]],
+    },
+    opts = {
+      hl = "Type",
+      shrink_margin = false,
+      -- wrap = "overflow";
+    },
+  }
+  startify.section.top_buttons = {
+    type = "group",
+    val = {
+      startify.button("i", "New file", "<cmd>ene <CR>"),
+    },
+  }
+  local config = {
+    layout = {
+      { type = "padding", val = 1 },
+      startify.section.header,
+      { type = "padding", val = 2 },
+      startify.section.top_buttons,
+      startify.section.mru_cwd,
+      startify.section.mru,
+      { type = "padding", val = 1 },
+      startify.section.bottom_buttons,
+      startify.section.footer,
+    },
+    opts = {
+      margin = 3,
+      redraw_on_resize = false,
+      setup = function()
+        vim.api.nvim_create_autocmd("DirChanged", {
+          pattern = "*",
+          callback = function()
+            require("alpha").redraw()
+          end,
+        })
+      end,
+    },
+  }
+
+  alpha.setup(config)
 
   startify.section.bottom_buttons.val = {
     startify.button("v", "Neovim Config", ":e ~/.config/nvim/init.lua<cr>"),
