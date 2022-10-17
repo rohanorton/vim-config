e9c315d40c15b7365622f94f80866d064798de57
Rohan Orton <rohan.orton@gmail.com>
2022-10-18 01:56:41 +0200

    add some new postfix snippets for lua (experimenting)

--- snippets/lua.lua
+++ snippets/lua.lua
@@ -1,8 +1,10 @@
 local ls = require("luasnip")
 local s = ls.snippet
+local sn = ls.snippet_node
 local i = ls.insert_node
 local t = ls.text_node
 local f = ls.function_node
+local p = require("luasnip.extras.postfix").postfix
 
 local fmt = require("luasnip.extras.fmt").fmt
 
@@ -23,6 +25,8 @@ end)
   )
 )
 
+table.insert(snippets, describe)
+
 local it = s(
   "it",
   fmt(
@@ -34,7 +38,81 @@ end)
     { i(1, ""), i(2, "--") }
   )
 )
-table.insert(snippets, describe)
+
 table.insert(snippets, it)
+
+local dot_if = p(".if", {
+  d(1, function(_, parent)
+    return sn(
+      nil,
+      fmt(
+        [[
+if {} then
+  {}
+end
+]],
+        { i(1, parent.env.POSTFIX_MATCH), i(2, "") }
+      )
+    )
+  end),
+})
+
+table.insert(snippets, dot_if)
+
+local dot_else = p(".else", {
+  d(1, function(_, parent)
+    return sn(
+      nil,
+      fmt(
+        [[
+if not {} then
+  {}
+end
+]],
+        { i(1, parent.env.POSTFIX_MATCH), i(2, "") }
+      )
+    )
+  end),
+})
+
+table.insert(snippets, dot_else)
+
+local dot_fn = p(".fn", {
+  d(1, function(_, parent)
+    return sn(
+      nil,
+      fmt(
+        [[
+local function {}({})
+  {}
+end
+]],
+        { i(1, parent.env.POSTFIX_MATCH), i(2, ""), i(3, "--") }
+      )
+    )
+  end),
+})
+
+table.insert(snippets, dot_fn)
+
+local dot_br = p(".br", {
+  l("{" .. l.POSTFIX_MATCH .. "}"),
+})
+
+table.insert(snippets, dot_br)
+
+local dot_par = p(".par", {
+  l("(" .. l.POSTFIX_MATCH .. ")"),
+})
+
+table.insert(snippets, dot_par)
+
+local dot_var = p(".var", {
+  d(1, function(_, parent)
+    return sn(nil, fmt("local {} = {}", { i(1, "variable_name"), i(2, parent.env.POSTFIX_MATCH) }))
+  end),
+})
+
+table.insert(snippets, dot_var)
 
 return snippets, autosnippets
