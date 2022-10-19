edd7a39d6d4eaea7906917f7c6bfd49341eca557
Rohan Orton <rohan.orton@gmail.com>
2022-10-19 21:09:03 +0200

    add safe_require to colorscheme file

--- lua/rohan/colorscheme.lua
+++ lua/rohan/colorscheme.lua
@@ -1,29 +1,26 @@
-local status_ok, gruvbox = pcall(require, "gruvbox")
-if not status_ok then
-  return
-end
+SAFE_REQUIRE("gruvbox", function(gruvbox)
+  -- Setup must be called before loading the colorscheme
+  gruvbox.setup({
+    -- Add custom overrides here
+    overrides = {
+      -- E.G.
+      -- SignColumn = {bg = "#ff9900"}
+    },
 
--- Setup must be called before loading the colorscheme
-gruvbox.setup({
-  -- Add custom overrides here
-  overrides = {
-    -- E.G.
-    -- SignColumn = {bg = "#ff9900"}
-  },
+    -- Defaults...
+    undercurl = true,
+    underline = true,
+    bold = true,
+    italic = true,
+    strikethrough = true,
+    invert_selection = false,
+    invert_signs = false,
+    invert_tabline = false,
+    invert_intend_guides = false,
+    inverse = true, -- invert background for search, diffs, statuslines and errors
+    contrast = "", -- can be "hard", "soft" or empty string
+  })
 
-  -- Defaults...
-  undercurl = true,
-  underline = true,
-  bold = true,
-  italic = true,
-  strikethrough = true,
-  invert_selection = false,
-  invert_signs = false,
-  invert_tabline = false,
-  invert_intend_guides = false,
-  inverse = true, -- invert background for search, diffs, statuslines and errors
-  contrast = "", -- can be "hard", "soft" or empty string
-})
-
--- Load colorscheme
-vim.cmd("colorscheme gruvbox")
+  -- Load colorscheme
+  vim.cmd("colorscheme gruvbox")
+end)
