0b68665d5a4f1d79b4b8ee3703f22191d53d7dbc
Rohan Orton <rohan.orton@gmail.com>
2022-10-20 03:51:54 +0200

    Add variable to easily turn off formatting

--- lua/rohan/format.lua
+++ lua/rohan/format.lua
@@ -1,12 +1,18 @@
 local Set = require("rohan.util.set")
 
 -- Prevent certain clients from formatting
+vim.g.rohan__formatters_enabled = true
+-- Prevent certain clients from formatting
 vim.g.rohan__excluded_formatters = {
   "tsserver",
   "sumneko_lua",
 }
 
 local function format()
+  if not vim.g.rohan__formatters_enabled then
+    return
+  end
+
   local excluded = Set(vim.g.rohan__excluded_formatters or {})
 
   vim.lsp.buf.format({
