41202b2235c6fee2883db7873db74153d1998247
Rohan Orton <ro@itacs.de>
2023-04-04 13:23:54 +0200

    change mytime directory
    previously using dropbox, but i'm cheap so using icloud for now

--- plugin/mytime.lua
+++ plugin/mytime.lua
@@ -1,5 +1,7 @@
 SAFE_REQUIRE({ "mytime" }, function(mytime)
   mytime.setup({
-    directory = vim.fn.resolve(os.getenv("HOME") .. "/Dropbox/rohan"),
+    directory = vim.fn.resolve(
+      os.getenv("HOME") .. "/Library/Mobile Documents/com~apple~CloudDocs/Documents/Obsidian/rohan"
+    ),
   })
 end)
